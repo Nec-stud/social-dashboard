41d27b8082a9d6648163cbe506777d6aa2326e41
Gabin <dev@nec-stud.fr>
2024-11-15 14:50:04 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
